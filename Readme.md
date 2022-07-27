f583095beb0d0e9128994a3ed46bf20b88acf2cc
Alvaro Quena <alvaro.quena1@gmail.com>
2022-07-27 15:46:10 -0400

    Revert "Create Action Runner Machine"
    This reverts commit 114c8bdb009ad2c5a7cb37177f30004095a2354b.

--- Readme.md
+++
@@ -1,1 +0,0 @@
-#Readme Wordepress post
--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Java
-apt update && apt upgrade -y
-apt install default-jdk -y
-# Install pip python
-apt install python3-pip -y
-# Install docker-compose
-apt install docker-compose -y
